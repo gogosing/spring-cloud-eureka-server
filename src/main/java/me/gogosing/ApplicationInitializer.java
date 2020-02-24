fe5392c7b6fc36ca822593b5265647524d92ad0f
JinBum Jeong <gogosing@naver.com>
2020-02-24 13:25:36 +0900

    초기 commit.

---
+++ src/main/java/me/gogosing/ApplicationInitializer.java
@@ -0,0 +1,17 @@
+package me.gogosing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by JinBum Jeong on 2020/02/22.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ApplicationInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationInitializer.class, args);
+    }
+}
